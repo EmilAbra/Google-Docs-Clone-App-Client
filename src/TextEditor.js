01685299ab8bed6f90c5010b807a70debe23b1eb
eabra <emab21@student.bth.se>
2024-02-22 09:43:51 +0100

    Finished App

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -3,7 +3,9 @@ import React from 'react'
 import Quill from 'quill';
 import "quill/dist/quill.snow.css"
 import { io } from 'socket.io-client'
+import { useParams } from 'react-router-dom';
 
+const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -19,6 +21,7 @@ const TOOLBAR_OPTIONS = [
 export default function TextEditor() {
   const [socket, setSocket] = useState()
   const [quill, setQuill] = useState()
+  const { id: documentId } = useParams()
 
   useEffect(() => {
     const s = io("http://localhost:3001")
@@ -28,6 +31,17 @@ export default function TextEditor() {
       s.disconnect()
     }
   }, [])
+
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    socket.once("load-document", document => {
+      quill.setContents(document)
+      quill.enable()
+    })
+
+    socket.emit("get-document", documentId)
+  }, [socket, quill, documentId])
 
   useEffect(() => {
     if (socket == null || quill == null) return
@@ -57,6 +71,18 @@ export default function TextEditor() {
     }
   }, [socket, quill])
 
+  useEffect(() => {
+    if (socket == null || quill == null) return
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents())
+    }, SAVE_INTERVAL_MS)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [socket, quill])
+
   const wrapperRef = useCallback(wrapper => {
     if (wrapper == null) return
 
@@ -67,8 +93,8 @@ export default function TextEditor() {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
     })
-    // q.disable()
-    // q.setText("Loading...")
+    q.disable()
+    q.setText("Loading...")
     setQuill(q)
   }, [])
 
